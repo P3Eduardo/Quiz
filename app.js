6a6f1f3d14b58952caac60e76079be3efd15a956
Eduardo Pena <p3eduardo0@gmail.com>
2018-07-31 09:47:46 -0400

    Quiz

---
+++ app.js
@@ -0,0 +1,63 @@
+// Task 1:
+function sendAlert() {
+console.log("Hello World");
+
+//Task 2
+let fname = "Eduardo"
+let lname = "Pena"
+console.log(lname,fname);
+
+//Task 3
+let x = 18
+let y = 19
+if (x===18) {
+let x = 19
+let y = 18
+console.log(x,y);
+}
+
+//Task 4
+let isOn = 0
+let lightUp = 0
+
+if (isOn && !lightUp || isOn && lightUp )
+{
+console.log("Faulty Switch");
+}
+else
+{
+console.log("Working Correctly");
+}
+// The message "Working Correctly will display when both boolean variables are 0."
+
+
+//Task 5
+let z = 1.0003423830
+let c= 1.0013248857
+if (x>y) {
+  console.log(z);
+}
+else {
+ console.log(c);
+}
+
+//Task 6
+let num = -30+70
+
+if (-50>=num<=50) {
+console.log(true);
+
+}
+else{
+    console.log(false);
+}
+//Task 7
+for (let i = 1;i <= 20; i = 2 * i + 3) {
+
+console.log(3 * 1);
+}
+//The Value will still be 3
+
+
+
+}
