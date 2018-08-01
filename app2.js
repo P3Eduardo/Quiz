6ee442b229af2dc3235efb91503beb1772a71508
Eduardo Pena <p3eduardo0@gmail.com>
2018-08-01 09:31:00 -0400

    Quiz2 Redo

---
+++ app2.js
@@ -0,0 +1,83 @@
+// Task 1:
+function sendAlert() {
+console.log("I will succeed");
+
+//Task 2
+let fname = "Eduardo"
+let lname = "Pena"
+console.log(lname,fname);
+
+//Task 3
+let x = 18
+let y = 19
+if (x===18) {
+let x = 19
+let y = 18
+console.log(x,y);
+}
+
+//Task 4
+let hasAccess = 0
+let hasPass = 0
+
+if (hasAccess && !hassPass || hasAccess && hassPass )
+{
+console.log("I am in");
+}
+else
+{
+console.log("I am locked out");
+}
+// The message "Working Correctly will display when both boolean variables are 0."
+
+
+//Task 5
+let z = 1.0003423830
+let c= 1.0013248857
+if (z>c) {
+  console.log(z);
+}
+else {
+ console.log(c);
+}
+
+//Task 6
+let num = -30+70
+
+if (-50>=num<=50) {
+console.log(true);
+
+}
+else{
+    console.log(false);
+}
+//Task 7
+for (let i = 1;i <= 50; i = 3 * i + 2) {
+
+console.log(2 * i + i);
+}
+
+//The Value will still be 51
+
+//Task 8
+for (let e = 8;e <= 68; e = e + 1) {
+console.log(e);
+}
+//Task 9
+for (let q = 11;q <=79; q = q + 1)
+{
+    if ( q!=22 || q!=56 ) {
+       console.log(q);
+}
+
+//Task 10
+ for (let a = 100;a > 2; a = a - 2)
+ {
+console.log(a);
+
+}
+
+}
+
+
+}
